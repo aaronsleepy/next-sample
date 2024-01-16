016bf514907469ee674968ae05ea7e4149c22f73
Aaron Song <aaron@kmong.com>
2024-01-16 19:42:53 +0900

    ui: API 라우터 사용 예제

---
+++ pages/sayHello.tsx
@@ -0,0 +1,24 @@
+/**
+ * API 라우터를 사용하는 예제
+ */
+
+import { useEffect, useState } from "react";
+
+function SayHello() {
+    const [data, setData] = useState({name: ''})
+    useEffect(() => {
+        fetch('api/hello')
+            .then((res) => res.json())
+            .then((profile) => {
+                setData(profile)
+            })
+    }, [])
+
+    return (
+        <div>
+            hello {data.name}
+        </div>
+    )
+}
+
+export default SayHello
